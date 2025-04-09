9d29985a2ce06409d53d0c594ee9db8fbef6ef05
Andres Tenesaca <jtenesaca74@gmail.com>
2025-04-09 16:18:25 -0500

    first commit

---
+++ src/icc/est/u1/recursividad/IccEstU1Recursividad.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package icc.est.u1.recursividad;
+
+/**
+ *
+ * @author atene
+ */
+public class IccEstU1Recursividad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n=5;
+        int resultadoFinal=factorial(n);
+    }
+    public static int factorial(int n){
+        if (n==0) {
+            System.out.println("alcanze el caso base");
+            return 1;
+        }
+        int resultado=n*factorial(n-1);
+        System.out.println("calcular factorial de " +n+ " * "+" (Factorial) ["+(n-1)+"]" +" = "+resultado );
+        return resultado;
+    }
+    
+}
